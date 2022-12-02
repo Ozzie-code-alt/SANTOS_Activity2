9263e623d8f62ab48ae1fc756abefb51609d768d
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2022-12-02 19:35:36 +0800

    OnlyCommit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http') // adds http library to variable
+const port = 3000 
+
+
+const server = http.createServer(function(req, res){
+res.write("Hello World")
+res.end()
+})
+
+server.listen(port,function(error){
+    if(error){
+        console.log('Something went wrong', error)
+    }
+    else{
+        console.log("Server is Listening on port" + port)
+    }
+})
